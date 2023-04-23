88804953d6f841ea949202f4faa14b92f0427709
Roshan Sah <roshansah400@gmail.com>
2023-04-23 11:02:37 +0530

    added all files

--- src/main/java/com/pms/G2NotificationServiceApplication.java
+++ src/main/java/com/pms/G2NotificationServiceApplication.java
@@ -2,7 +2,6 @@ package com.pms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 import com.pms.exception.GlobalExceptionHandler;
